397beb4eeadf69575b5d72b00afc0c5cf6c7b847
v13rc <sunkernel@proton.me>
2024-10-07 15:13:07 +0200

    Update monitor_server.py

--- monitor_server.py
+++ monitor_server.py
@@ -494,48 +494,54 @@ def display_validators():
         </table>
 
        <!-- Połączona tabela z walidatorami i blokami -->
-        <table style="width: 100%; table-layout: fixed;">
-            <tr>
-                <th style="width: 10%;">#</th>
-                <th style="width: 40%;">Validators in Quorum</th>
-                <th style="width: 10%;">Block Height</th>
-                <th style="width: 40%;">Proposer</th>
-            </tr>
-            {% for i in range(max_length) %}
-            <tr>
-                <td>{{ i + 1 }}</td>
-        
-                <!-- Kolumna z walidatorami (odwrócona kolejność) -->
-                <td>
-                    {% if i < validators_in_quorum|length %}
-                        <span class="{{ 'validator-in-quorum' if validators_in_quorum[i] in protx_in_second_table else '' }} {{ 'highlight-latest' if validators_in_quorum[i] == latest_block_validator else '' }}">
-                            {{ validators_in_quorum[i] }}
-                        </span>
-                    {% else %}
-                        &nbsp;
-                    {% endif %}
-                </td>
-        
-                <!-- Kolumna z wysokościami bloków -->
-                <td>
-                    {% if i < displayed_blocks|length %}
-                        {{ displayed_blocks[i].height }}
-                    {% else %}
-                        &nbsp;
-                    {% endif %}
-                </td>
-        
-                <!-- Kolumna z proposerami bloków -->
-                <td>
-                    {% if i < displayed_blocks|length %}
-                        {{ displayed_blocks[i].proposer_pro_tx_hash }}
-                    {% else %}
-                        &nbsp;
-                    {% endif %}
-                </td>
-            </tr>
-            {% endfor %}
-        </table>
+    <!-- Połączona tabela z walidatorami i blokami -->
+    <table style="width: 100%; table-layout: fixed;">
+        <tr>
+            <th style="width: 10%;">#</th>
+            <th style="width: 40%;">Validators in Quorum</th>
+            <th style="width: 10%;">Block Height</th>
+            <th style="width: 40%;">Proposer</th>
+        </tr>
+        {% for i in range(max_length) %}
+        <tr>
+            <td>{{ i + 1 }}</td>
+    
+            <!-- Kolumna z walidatorami (odwrócona kolejność) -->
+            <td>
+                {% if i < validators_in_quorum|length %}
+                    <span class="{{ 'validator-in-quorum' if validators_in_quorum[i] in protx_in_second_table else '' }} {{ 'highlight-latest' if validators_in_quorum[i] == latest_block_validator else '' }}">
+                        {{ validators_in_quorum[i] }}
+                    </span>
+                {% else %}
+                    &nbsp;
+                {% endif %}
+            </td>
+    
+            <!-- Kolumna z wysokościami bloków -->
+            <td>
+                {% if i < displayed_blocks|length %}
+                    {{ displayed_blocks[i].height }}
+                {% else %}
+                    &nbsp;
+                {% endif %}
+            </td>
+    
+            <!-- Kolumna z proposerami bloków -->
+            <td>
+                {% if i < displayed_blocks|length %}
+                    {% set proposer = displayed_blocks[i].proposer_pro_tx_hash %}
+                    {% set server_pro_tx = heartbeat_data[selected_server_name].proTxHash %}
+                    <span class="{% if proposer == server_pro_tx %}green bold{% endif %}">
+                        {{ proposer }}
+                    </span>
+                {% else %}
+                    &nbsp;
+                {% endif %}
+            </td>
+        </tr>
+        {% endfor %}
+    </table>
+
 
     </body>
     </html>
