5b054d2378deb7cffd85640eb4aba09798b26671
v13rc <sunkernel@proton.me>
2024-11-04 11:09:57 +0100

    Update vm_config.sh

--- vm_config.sh
+++ vm_config.sh
@@ -18,6 +18,7 @@ apt install -y ufw fail2ban htop nano iputils-ping openvpn jq cron
 ufw allow 1194/udp
 ufw allow 1195/udp
 ufw allow 1196/udp
+ufw allow 1197/udp
 ufw allow 443
 ufw allow 80
 ufw allow 9999
