52b128733aae3a1a53c69f85de5fdbfbf8622074
v13rc <sunkernel@proton.me>
2025-01-15 10:21:58 +0100

    Update vm_config.sh

--- vm_config.sh
+++ vm_config.sh
@@ -15,16 +15,7 @@ apt install -y ufw fail2ban htop nano iputils-ping openvpn jq cron
 [ $? -eq 0 ] && echo -e "${GREEN}Success install packages${NC}" || echo -e "${RED}Failed install packages${NC}"
 
 #Net configuration
-ufw allow 1190/udp
-ufw allow 1191/udp
-ufw allow 1192/udp
-ufw allow 1193/udp
-ufw allow 1194/udp
-ufw allow 1195/udp
-ufw allow 1196/udp
-ufw allow 1197/udp
-ufw allow 1198/udp
-ufw allow 1199/udp
+#ufw allow 1194/udp
 ufw allow 443
 ufw allow 80
 ufw allow 9999
